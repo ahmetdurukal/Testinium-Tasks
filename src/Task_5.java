137614dd05bc9e454b2e141fe975ac9bc5c62727
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-05 10:41:00 +0300

    Testinium Task 5 (String array)

---
+++ src/Task_5.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Task_5 {
+    public static void main(String[] args){
+        String [] sehirler=new String[5];
+        sehirler[0]="Bursa";
+        sehirler[1]="İstanbul";
+        sehirler[2]="Ankara";
+        sehirler[3]="Adana";
+        sehirler[4]="İzmir";
+        System.out.println("Array'in ilk elemanı: "+sehirler[0]);
+        System.out.println("son elemanı: "+sehirler[sehirler.length-1]);
+        System.out.println("Dizi uzunluğu: "+sehirler.length);
+
+    }
+}
