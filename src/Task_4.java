caadb9c1eafc3ba64a7b1d33e212814192721840
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-05 08:15:14 +0300

    Testinium Task 4 (veriable rename)

--- src/Task_4.java
+++ src/Task_4.java
@@ -5,20 +5,20 @@ public class Task_4 {
     public static void main(String[] args) {
 
         Random rand = new Random();
-        int rand_int = rand.nextInt(10);
-        System.out.println(rand_int + "Kod yapısı 0'dan 10'a kadar bir sayı tuttu sayıyı bulmak için 3 hakkınız var.");
+        int randInt = rand.nextInt(10);
+        System.out.println(randInt + "Kod yapısı 0'dan 10'a kadar bir sayı tuttu sayıyı bulmak için 3 hakkınız var.");
         int hak = 3;
         do {
             System.out.println("Kalan hakkınız: " + hak);
             Scanner input = new Scanner(System.in);
             int number = input.nextInt();
-            if (number == rand_int) {
-                System.out.println("Tebrikler! Doğru tahmin. Sayımız: " + rand_int);
+            if (number == randInt) {
+                System.out.println("Tebrikler! Doğru tahmin. Sayımız: " + randInt);
                 break;
-            } else if (number != rand_int & hak > 1) {
+            } else if (number != randInt & hak > 1) {
                 System.out.println("Yanlış tahmin.");
             } else {
-                System.out.println("Maalesef hakkınız doldu. Sayımız: " + rand_int);
+                System.out.println("Maalesef hakkınız doldu. Sayımız: " + randInt);
             }
             hak--;
         }
@@ -29,14 +29,14 @@ public class Task_4 {
             System.out.println("Kalan hakkınız: "+hak);
             Scanner input = new Scanner(System.in);
             int number = input.nextInt();
-            if (number == rand_int) {
-                System.out.println("Tebrikler! Doğru tahmin. Sayımız: "+rand_int);
+            if (number == randInt) {
+                System.out.println("Tebrikler! Doğru tahmin. Sayımız: "+randInt);
                 break;
-            } else if (number != rand_int & hak > 1) {
+            } else if (number != randInt & hak > 1) {
                 System.out.println("Yanlış tahmin.");
             }
             else  {
-                System.out.println("Maalesef hakkınız doldu. Sayımız: "+rand_int);
+                System.out.println("Maalesef hakkınız doldu. Sayımız: "+randInt);
             }
             hak--;
         }
@@ -45,14 +45,14 @@ public class Task_4 {
             System.out.println("Kalan hakkınız: "+hak);
             Scanner input = new Scanner(System.in);
             int number = input.nextInt();
-            if (number == rand_int) {
-                System.out.println("Tebrikler! Doğru tahmin. Sayımız: "+rand_int);
+            if (number == randInt) {
+                System.out.println("Tebrikler! Doğru tahmin. Sayımız: "+randInt);
                 break;
-            } else if (number != rand_int & hak > 1) {
+            } else if (number != randInt & hak > 1) {
                 System.out.println("Yanlış tahmin.");
             }
             else  {
-                System.out.println("Maalesef hakkınız doldu. Sayımız: "+rand_int);
+                System.out.println("Maalesef hakkınız doldu. Sayımız: "+randInt);
             }*/
     }
 }
