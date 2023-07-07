ddb61cb823b8e47852750f33927065b7fdf4835b
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-07 12:01:31 +0300

    Reverse List

--- src/Task10.java
+++ src/Task10.java
@@ -2,9 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 public class Task10 {
     public static void main(String[] args){
-        //elimizde bir liste var ister integer ister
-        // string olsun mesela  ahmet nazım mehmet diye olsun
-        // bunu mehmet nazım ahmet nasıl yaparız
         List<String> reverseList=new ArrayList<>();
         String[] list={"Ahmet","Nazım","Mehmet"};
         for (String i:list){
