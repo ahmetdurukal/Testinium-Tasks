9d8fef3f5ca495d47c00593b0f7044cf124c53dc
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-11 16:35:58 +0300

    Ex. using Optional

---
+++ src/Task21.java
@@ -0,0 +1,20 @@
+import javax.print.attribute.standard.PrinterName;
+import java.util.Optional;
+
+public class Task21 {
+    public static void main(String[] args) {
+        Optional<String> gender = Optional.of("Male");
+        String name = null;
+        printName("folsdev");
+    }
+    public static void printName(String name){
+        Optional<String> nameOptional = Optional.ofNullable(name);
+
+
+        System.out.println(
+                nameOptional
+                        .map(String::toUpperCase)//Optional<String>
+                        .orElse("")//FOLSDEV
+        );
+    }
+}
