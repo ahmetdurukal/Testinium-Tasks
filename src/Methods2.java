e0f994081e70639d88dd8ffd0b63fee70f3ed263
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-13 11:03:26 +0300

    Methods

--- src/Methods2.java
+++ src/Methods2.java
@@ -3,9 +3,11 @@ public class Methods2 {
         String message = "Bugün hava çok güzel.";
         String newMesage= getCities();
         int number = collection(5,7);
+        int number2= collection2(1,2,3,4,5);
         System.out.println(message);
         System.out.println(newMesage);
         System.out.println(number);
+        System.out.println(number2);
     }
     public static void add(){
         System.out.println("Eklendi");
@@ -18,6 +20,14 @@ public class Methods2 {
     }
     public static int collection (int number1,int number2){
         return number2+number1;
+    }
+    public static int collection2 (int... numbers){
+        int total=0;
+        for (int number:numbers){
+            total+=number;
+
+        }
+        return total;
     }
     public static String getCities() {
     return "Ankara";
