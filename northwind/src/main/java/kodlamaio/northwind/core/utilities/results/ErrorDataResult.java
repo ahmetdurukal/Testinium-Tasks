057be25bf58747d5d62afe88f8cd6d27562e80f0
Ahmet Mahmut Durukal <ahmetmahmutdurukal@gmail.com>
2023-07-29 22:20:04 +0300

    update

--- northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorDataResult.java
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorDataResult.java
@@ -2,7 +2,7 @@ package kodlamaio.northwind.core.utilities.results;
 
 import javax.xml.crypto.Data;
 
-public class ErrorDataResult<T> extends DataResult {
+public class ErrorDataResult<T> extends DataResult<T> {
     public ErrorDataResult(T data, String message) {
         super(data,false, message);
     }
